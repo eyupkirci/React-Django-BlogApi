f8e1214570fbaabc741fa4324737ac8c714a78c1
eyupkirci <61272579+eyupkirci@users.noreply.github.com>
2021-12-03 02:19:05 +0300

    blog_api serializer created

---
+++ blog_api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from .models import *
+from django.db.models import fields
+from django.db import models
+
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields ='__all__'
