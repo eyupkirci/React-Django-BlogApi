c0897165b1bc6ce707f1cdfd6e14118e8a9ea102
eyupkirci <61272579+eyupkirci@users.noreply.github.com>
2021-12-07 01:16:35 +0300

    Accounts api fixed

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers, validators
-
 from django.contrib.auth.models import User
 from django.contrib.auth.password_validation import validate_password
 from django.contrib.auth.hashers import make_password
